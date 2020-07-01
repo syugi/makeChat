4d2da04874bf49ceeb5d9401ac0ee1cdbf08ab3b
a슈기 <altnr23@gmail.com>
2020-07-01 06:13:42 +0000

    Fix saveUser error

--- views/user.js
+++ views/user.js
@@ -48,7 +48,7 @@ function signUp(){
             <div class="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
                 <div class="bg-white px-6 py-8 rounded shadow-md text-black w-full">
                     <h1 class="mb-8 text-3xl text-center">Sign up</h1>
-                    <form class="" method="POST" onsubmit="return validate();" action="/login/saveUser" >
+                    <form class="" method="POST" onsubmit="return validate();" action="/user/saveUser" >
 											<input 
 													type="text"
 													class="block border border-grey-light w-full p-3 rounded mb-4"
